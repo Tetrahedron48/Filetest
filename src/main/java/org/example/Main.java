6c8cb50d996fb7440c9b73ca6237bbaf8e96d96e
Yuxuan Shen <156739596+Tetrahedron48@users.noreply.github.com>
2024-04-02 11:39:00 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,31 @@ public class Main {
         String line;
         Double sum= (double) 0;
         String filepath = "C:\\Users\\86185\\Desktop\\Groceries.txt";
-
-          FileReader  fileReader = new FileReader(filepath);
-
+        String writerpath="C:\\Users\\86185\\Desktop\\GroceriesFormatted.txt";
+        FileReader  fileReader = new FileReader(filepath);
         BufferedReader reader = new BufferedReader(fileReader);
+        FileWriter fileWriter=new FileWriter(writerpath);
+        BufferedWriter writer=new BufferedWriter(fileWriter);
+        writer.write("****************************************************\n");
+        writer.write("ID#        Item                   Quantity        Price (€)\n");
         while((line=reader.readLine())!=null){
             System.out.println(line);
             String [] a=line.split(",");
-            Double n= Double.valueOf(a[3]);
-            sum+=n;
+            Double price= Double.valueOf(a[3]);
+            String id=a[0];
+            String item=a[1];
+            String quantity=a[2];
+            sum+=price;
+            writer.write(id+"\t\t"+item+ "\t\t" +quantity+"\t\t"+price+'\n');
         }
+        writer.write("****************************************************\n");
+
         String format = String.format("%.2f", sum);
         System.out.println("Total price:"+format);
+        writer.write("The grocery shopping total is: €"+format);
+        writer.write("\n****************************************************\n");
         reader.close();
+        writer.flush();
+        writer.close();
     }
 }
